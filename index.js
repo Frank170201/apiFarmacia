9c0b40abe598efc5e8c2ffd0e6d3dbc753974a2d
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-05 20:32:06 -0600

    Agregando cambios

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const Product = require('./modules/product.model.js');
+
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello this is the pincipal response')
+});
+
+app.get('/api/products',async (req,res)=>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/product/:id',async (req,res)=>{
+    try {
+        const {id} = req.params
+        const productById = await Product.findById(id)
+        res.status(200).json(productById);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/products',async (req,res)=>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+
+mongoose.connect('mongodb+srv://sm19035:hFMxSrzawumkXpeQ@cluster0.7oovc.mongodb.net/Api-Farmacia?retryWrites=true&w=majority&appName=Cluster0')
+    .then(()=> {
+        console.log('DB is connected')
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+        
+    })
+    .catch((err)=>{
+        console.log('Connection failed')
+    });
