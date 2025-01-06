5145c0ca4da5d09d4d344c8e5c584194ebcce067
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-06 09:33:32 -0600

    agregando cambios

--- index.js
+++ index.js
@@ -39,7 +39,35 @@ app.post('/api/products',async (req,res)=>{
     }
 })
 
+//update a product
+app.put('/api/product/:id',async (req,res)=>{
+    try {
+        const {id} = req.params
+        const product=await Product.findByIdAndUpdate(id,req.body);
+        if(!product){
+            res.status(404).json({message: 'Product not found'})
+        }
+        const updatedProduct= await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
+//delete a product
+
+app.delete('/api/product/:id',async (req,res)=>{
+    try {
+        const {id} = req.params
+        const product=await Product.findByIdAndDelete(id);
+        if(!product){
+            res.status(404).json({message: 'Product not found'})
+        }
+        res.status(200).json({message: 'Product deleted'})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect('mongodb+srv://sm19035:hFMxSrzawumkXpeQ@cluster0.7oovc.mongodb.net/Api-Farmacia?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=> {
